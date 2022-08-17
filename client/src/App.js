ef0f8d5e1eea023bbc09cae2d4759f50cc60dbce
Uli-mgh <ulicmgh@gmail.com>
2022-08-17 08:45:15 -0300

    mistakes were made

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import BottomNav from "./components/BottomNav";
+import Loading from "./components/Loading";
+import NavBar from "./components/NavBar";
+import Notification from "./components/Notification";
+import Login from "./components/user/Login";
+
+const App = () => {
+  return (
+    <>
+      <Loading />
+      <Notification />
+      <Login />
+      <NavBar />
+      <BottomNav />
+    </>
+  );
+};
+
+export default App;
